5de51e61699d3c8b57e42e59fb6ca958f42ba29f
NakePranob <pranob.onphaeng@gmail.com>
2024-08-19 21:48:20 +0700

    edit

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,70 +1,68 @@
-import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
+import NextAuth from "next-auth"
+import CredentialsProviders from "next-auth/providers/credentials"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
 import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
 declare module "next-auth" {
     interface Session {
-        user: Users | null | undefined;
+      user: Users | null | undefined;
     }
 
     interface User {
-        id: any | null;
-        email: string;
-        name: string | null | undefined;
-        image: any | null;
-        roleId: any | null;
+        id:     any | null
+        email:  string 
+        name:   string  | null | undefined
+        image:  any | null
+        roleId: any | null 
     }
 
     interface JWT {
-        id: any | null;
-        email: string;
-        name: string | null | undefined;
-        image: string | null;
-        roleId: string;
+        id:     any | null
+        email:  string 
+        name: string  | null | undefined
+        image: string  | null
+        roleId: string 
     }
 }
 
 interface Users {
-    id: any | null;
-    email: string;
-    name: string | null | undefined;
-    image: any | null;
-    roleId: any | null;
+    id:     any | null
+    email:  string 
+    name:   string  | null | undefined
+    image:  any | null
+    roleId: any | null
 }
 
 const handler = NextAuth({
     providers: [
-        CredentialsProvider({
+        CredentialsProviders({
             name: "Credentials",
             credentials: {
                 email: { label: "Email", type: "email", placeholder: "email.address@email.com" },
                 password: { label: "Password", type: "password" },
             },
             async authorize(credentials) {
-                try {
-                    if (!credentials) return null;
-                    const user = await prisma.user.findUnique({
-                        where: { email: credentials.email },
-                    });
-
-                    if (user && await bcrypt.compare(credentials.password, user.password || '')) {
-                        return {
-                            id: `${user.id}`,
-                            email: user.email,
-                            name: user.firstname + ' ' + user.lastname,
-                            image: user.profile,
-                            roleId: user.roleId.toString(),
-                        };
-                    } else {
-                        throw new Error('อีเมลหรือรหัสผ่านไม่ถูกต้อง');
+                if (!credentials) return null;
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email,
+                    },
+                });
+                if (user && 
+                    (await bcrypt.compare(credentials.password, user.password?.toString() || ''))
+                ) {
+                    return {
+                        id: `${user.id}`,
+                        email: user.email,
+                        name: user.firstname+' '+user.lastname,
+                        image: user.profile,
+                        roleId: user.roleId.toString()
                     }
-                } catch (error) {
-                    console.error("Authorization Error: ", error);
-                    throw new Error('อีเมลหรือรหัสผ่านไม่ถูกต้อง');
+                } else {
+                    throw new Error('อีเมลหรือรหัสผ่านไม่ถูกต้อง')
                 }
             },
         }),
@@ -94,6 +92,6 @@ const handler = NextAuth({
             return session;
         },
     },
-});
+})
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
